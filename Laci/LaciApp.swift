c54a0e89f8534179b182f9239c7a03c87a7d32ff
febbryandika <febbryandika97@gmail.com>
2026-09-09 01:50:19 +0700

    Initial Commit

---
+++ Laci/LaciApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaciApp.swift
+//  Laci
+//
+//  Created by Febbry Andika on 09/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LaciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaciTests/LaciTests.swift
@@ -0,0 +1,19 @@
+//
+//  LaciTests.swift
+//  LaciTests
+//
+//  Created by Febbry Andika on 09/09/26.
+//
+
+import Testing
+@testable import Laci
+
+struct LaciTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
